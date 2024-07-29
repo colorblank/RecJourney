901b284e3fd83858769aa3e276843da464a86a92
whenowhere <jie05200@gmail.com>
2024-07-29 21:08:17 +0800

    📃 docs(model): add comments
    add model class comments

--- models/DIN/din.py
+++ models/DIN/din.py
@@ -26,6 +26,23 @@ class HeadArgs:
 
 
 class ActivationUnit(nn.Module):
+    """
+    ActivationUnit类继承自nn.Module，用于定义一个包含激活函数的神经网络单元。
+
+    参数:
+    - dim_in (int): 输入层的维度。
+    - dim_hidden (int): 隐藏层的维度。
+    - dim_out (int, 可选): 输出层的维度，默认为1。
+    - activation (str, 可选): 激活函数的类型，默认为"prelu"，可选值为"prelu"或"relu"。
+
+    Input:
+    - x_hist (Tensor): 历史序列的输入特征，形状为[batch_size, seq_len, dim_in]。
+    - x_cand (Tensor): 候选项的输入特征，形状为[batch_size, dim_in]。
+
+    Output:
+    - x (Tensor): 输出特征，形状为[batch_size, seq_len, 1]。
+    """
+
     def __init__(
         self, dim_in: int, dim_hidden: int, dim_out: int = 1, activation: str = "prelu"
     ) -> None:
@@ -56,6 +73,27 @@ class ActivationUnit(nn.Module):
 
 
 class DIN(nn.Module):
+    """
+    DIN类继承自nn.Module，用于定义一个深度信息融合网络（Deep Interest Network）模型。
+
+    参数:
+    - feat_args (FeatArgs): 特征参数。
+    - au_args (AUArgs): ActivationUnit参数。
+    - head_args (HeadArgs): 输出层的参数。
+    - activation (str, 可选): 激活函数的类型，默认为"prelu"，可选值为"prelu"或"relu"。
+    - dropout (float, 可选): 丢弃概率，默认为0.1。
+    - bias (bool, 可选): 是否使用偏置项，默认为True。
+
+    Input:
+    - x_profile (Tensor): 用户的输入特征，形状为[batch_size, dim]。
+    - x_hist (Tensor): 历史序列的输入特征，形状为[batch_size, seq_len, dim]。
+    - x_cand (Tensor): 候选项的输入特征，形状为[batch_size, dim]。
+    - x_context (Tensor): 上下文的输入特征，形状为[batch_size,dim]。
+
+    Output:
+    - y (Tensor): 输出特征，形状为[batch_size, 1]。
+    """
+
     def __init__(
         self,
         feat_args: FeatArgs,
@@ -87,18 +125,19 @@ class DIN(nn.Module):
         x_cand: Tensor,
         x_context: Tensor,
     ) -> Tensor:
-        """_summary_
-
+        """
         Arguments:
-            x_profile -- _description_
-            x_hist -- size [batch_size, seq_len, dim]
-            x_cand -- _description_
-            x_context -- _description_
-
-        Returns:
-            _description_
-        """        
-        w = self.au(x_hist, x_cand) # [batch_size, seq_len, 1]
+            x_profile -- a user profile feature.
+                shape: [batch_size, dim]
+            x_hist -- a list item features of history items.
+                each item feature is a tensor.
+                shape: [batch_size, seq_len, dim]
+            x_cand -- candicate item feature
+                shape: [batch_size, dim]
+            x_context -- context feature
+                shape: [batch_size, dim]
+        """
+        w = self.au(x_hist, x_cand)  # [batch_size, seq_len, 1]
 
         x = (x_hist * w).sum(dim=1)
         x = torch.cat([x_profile, x, x_context], dim=-1)
