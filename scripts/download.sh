78a9a825272d954cf4d7730ba541d6b7b96fd3b5
colorblank <jie05200@outlook.com>
2025-06-04 21:06:30 +0800

    refactor(scripts): 修改 KuaiRand-1K 下载脚本
    - 将数据目录从 ~/data/KuaiRand-1K 改为 ./data/
    - 更新解压目标路径以匹配新的目录结构

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,10 +1,10 @@
 wget https://zenodo.org/records/10439422/files/KuaiRand-1K.tar.gz 
 
 # 创建目标目录（如果不存在）
-mkdir -p ~/data/KuaiRand-1K
+mkdir -p ./data/
 
 # 解压到指定目录
-tar -xzvf KuaiRand-1K.tar.gz -C ~/data/KuaiRand-1K
+tar -xzvf KuaiRand-1K.tar.gz -C ./data/
 
 # 删除原始压缩包
 rm KuaiRand-1K.tar.gz
